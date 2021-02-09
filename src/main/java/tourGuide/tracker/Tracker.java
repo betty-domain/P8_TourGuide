625f98c4cf5cd1e551648e9d99d52e935173678f
betty Domain <betty.domain@free.fr>
2021-02-09 12:29:45 +0100

    remove jar references and include librairie to use StopWatch

--- src/main/java/tourGuide/tracker/Tracker.java
+++ src/main/java/tourGuide/tracker/Tracker.java
@@ -1,16 +1,15 @@
 package tourGuide.tracker;
 
+import org.apache.commons.lang3.time.StopWatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import tourGuide.service.TourGuideService;
+import tourGuide.user.User;
+
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.lang3.time.StopWatch;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import tourGuide.service.TourGuideService;
-import tourGuide.user.User;
 
 public class Tracker extends Thread {
 	private Logger logger = LoggerFactory.getLogger(Tracker.class);
@@ -56,7 +55,7 @@ public class Tracker extends Thread {
 
             tourGuideService.trackUserLocationForUserList(users);
 			stopWatch.stop();
-			logger.debug("Tracker Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds."); 
+			logger.debug("Tracker Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds.");
 			stopWatch.reset();
 			try {
 				logger.debug("Tracker sleeping");
